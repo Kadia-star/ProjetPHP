766c9bf7984ab9d8e69323fc08183404bf90e8e9
Kadidiatou Diallo <dkadidiatou619@gmail.com>
2025-03-03 10:12:53 +0100

    Initial commit

---
+++ test_db.php
@@ -0,0 +1,9 @@
+<?php
+require 'config.php';
+
+$sql = "SELECT COUNT(*) FROM produits";
+$stmt = $pdo->query($sql);
+$count = $stmt->fetchColumn();
+
+echo "La base de données est bien connectée ! Nombre de produits : " . $count;
+?>
